7da6714543269857b03291f3124f89abce6d1034
EC2 Default User <ec2-user@ip-172-31-21-236.us-east-2.compute.internal>
2021-03-29 14:35:27 +0000

    alteracoes para funcionar com arquivo dividido

--- first_tweet.py
+++ first_tweet.py
@@ -1,13 +1,13 @@
 import os
 import twitter
 
-f = open("pi_bot2/pi.txt", "r")
+f = open("pi_bot2/pi-00", "r")
 
 t = f.read(251)
 twitter.tweet("pi = " + t + "...")
 
 text = f.read()
 
-f = open("pi_bot2/pi.txt", "w")
+f = open("pi_bot2/pi-00", "w")
 f.write(text)
 f.close()
